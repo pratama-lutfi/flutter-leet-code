bbffe15a26de143d6bac1f60f5ed6bec8bc4bf31
pratama-lutfi <pratamalutfi60@gmail.com>
2025-11-11 18:05:16 +0700

    problem(remove-element): part 2

--- remove_element.dart
+++ remove_element.dart
@@ -1,13 +1,18 @@
 class Solution {
   int removeElement(List<int> nums, int val) {
-    final valLength = nums.where((n) => n == val).length;
-    print(valLength);
-    return 1;
+    final elementLength = nums.where((n) => n == val).length;
+    print(elementLength);
+    nums.removeWhere((n) => n == val);
+    print(nums);
+
+    return elementLength;
   }
 }
 
 void main() {
     Solution solution = Solution();
-    final result = solution.removeElement([3,2,2,3], 2);
+    // final result = solution.removeElement([3,2,2,3], 3);
+    
+    final result = solution.removeElement([0,1,2,2,3,0,4,2], 2);
     print(result);
 }
