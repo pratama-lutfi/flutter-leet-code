ef26f97e0e84510f924cf087a7f024e7d3705d82
pratama-lutfi <pratamalutfi60@gmail.com>
2025-11-12 11:27:12 +0700

    feat(index_occuring_string): init

---
+++ find_index_occuring_string.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int strStr(String haystack, String needle) {
+    
+  }
+}
+
+void main() {
+    Solution solution = Solution();
+    final result = solution.strStr();
+    print(result);
+}
